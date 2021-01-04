8f59c73cb04ca685bda9ed2882bbe0edd54918c1
LENOVO-THINKPAD\zk <18473088810@163.com>
2021-01-04 16:53:57 +0800

    提交暂存2

--- ProductOrderItemServiceImpl .java
+++ ProductOrderItemServiceImpl .java
@@ -1,3 +1,34 @@
+package com.zk.mall.service.Impl;
+
+import com.zk.mall.entity.ProductOrderItem;
+import com.zk.mall.mapper.ProductOrderItemMapper;
+import com.zk.mall.service.ProductOrderItemService;
+import com.zk.mall.util.PageUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+add func1
+
+@Service("productOrderItemService")
+public class ProductOrderItemServiceImpl implements ProductOrderItemService {
+    @Resource
+    ProductOrderItemMapper productOrderItemMapper;
+
+ 
+    @Override
+    public Integer getSaleCount(Integer productId) {
+        return productOrderItemMapper.selectSaleCount(productId);
+    }
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
+    @Override
+    public boolean add(ProductOrderItem productOrderItem) {
+        return productOrderItemMapper.insertOne(productOrderItem)>0;
+    }
+
 add func2
     @Transactional(propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
     @Override
